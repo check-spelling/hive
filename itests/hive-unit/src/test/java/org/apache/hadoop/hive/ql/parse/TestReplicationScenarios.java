490ddd0cfbff5a7bf521bbca4411dd54aad39825
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 09:47:27 -0500

    spelling: dropped
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenarios.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenarios.java
@@ -592,7 +592,7 @@ public class TestReplicationScenarios {
     // Table dropped after "repl dump"
     run("DROP TABLE " + dbName + ".unptned", driver);
 
-    // Partition droppped after "repl dump"
+    // Partition dropped after "repl dump"
     run("ALTER TABLE " + dbName + ".ptned " + "DROP PARTITION(b=1)", driver);
 
     run("REPL LOAD " + dbName + " INTO " + replDbName, driverMirror);
@@ -647,7 +647,7 @@ public class TestReplicationScenarios {
     // Table dropped after "repl dump"
     run("DROP TABLE " + dbName + ".unptned", driver);
 
-    // Partition droppped after "repl dump"
+    // Partition dropped after "repl dump"
     run("ALTER TABLE " + dbName + ".ptned " + "DROP PARTITION(b=1)", driver);
 
     run("REPL LOAD " + dbName + " INTO " + replDbName + lazyCopyClause, driverMirror);
