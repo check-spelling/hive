b3a2aed13f41fad71f564b8d4c8e81754cf549b7
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 09:43:40 -0500

    spelling: directory
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/java/org/apache/hadoop/hive/ql/exec/SkewJoinHandler.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/SkewJoinHandler.java
@@ -181,7 +181,7 @@ public class SkewJoinHandler {
       RowContainer<ArrayList<Object>> bigKey = (RowContainer)joinOp.storage[currBigKeyTag];
       Path outputPath = getOperatorOutputPath(specPath);
       FileSystem destFs = outputPath.getFileSystem(hconf);
-      bigKey.copyToDFSDirecory(destFs, outputPath);
+      bigKey.copyToDFSDirectory(destFs, outputPath);
 
       for (int i = 0; i < numAliases; i++) {
         if (((byte) i) == currBigKeyTag) {
@@ -191,7 +191,7 @@ public class SkewJoinHandler {
         if (values != null) {
           specPath = conf.getSmallKeysDirMap().get((byte) currBigKeyTag).get(
               (byte) i);
-          values.copyToDFSDirecory(destFs, getOperatorOutputPath(specPath));
+          values.copyToDFSDirectory(destFs, getOperatorOutputPath(specPath));
         }
       }
     }
