eed0e2691ae81b7a4c73a768da761f96475e0922
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 19:03:55 -0500

    spelling: kerberos
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/HMSHandler.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/HMSHandler.java
@@ -402,7 +402,7 @@ public class HMSHandler extends FacebookBase implements IHMSHandler {
   }
 
   // This will return null if the metastore is not being accessed from a metastore Thrift server,
-  // or if the TTransport being used to connect is not an instance of TSocket, or if kereberos
+  // or if the TTransport being used to connect is not an instance of TSocket, or if kerberos
   // is used
   static String getThreadLocalIpAddress() {
     return threadLocalIpAddress.get();
