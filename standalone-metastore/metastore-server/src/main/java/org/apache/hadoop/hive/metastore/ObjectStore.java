d4345f2f8a8e16eed00a23124a3bb617a80c0dea
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 21:06:48 -0500

    spelling: transaction
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ standalone-metastore/metastore-server/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -310,7 +310,7 @@ public class ObjectStore implements RawStore, Configurable {
   private DatabaseProduct dbType = null;
   private PartitionExpressionProxy expressionProxy = null;
   private Configuration conf;
-  private volatile int openTrasactionCalls = 0;
+  private volatile int openTransactionCalls = 0;
   private Transaction currentTransaction = null;
   private TXN_STATUS transactionStatus = TXN_STATUS.NO_STATE;
   private Pattern partitionValidationPattern;
@@ -346,7 +346,7 @@ public class ObjectStore implements RawStore, Configurable {
     pm = null;
     directSql = null;
     expressionProxy = null;
-    openTrasactionCalls = 0;
+    openTransactionCalls = 0;
     currentTransaction = null;
     transactionStatus = TXN_STATUS.NO_STATE;
 
@@ -544,8 +544,8 @@ public class ObjectStore implements RawStore, Configurable {
 
   @Override
   public boolean openTransaction() {
-    openTrasactionCalls++;
-    if (openTrasactionCalls == 1) {
+    openTransactionCalls++;
+    if (openTransactionCalls == 1) {
       currentTransaction = pm.currentTransaction();
       currentTransaction.begin();
       transactionStatus = TXN_STATUS.OPEN;
@@ -559,7 +559,7 @@ public class ObjectStore implements RawStore, Configurable {
     }
 
     boolean result = currentTransaction.isActive();
-    debugLog("Open transaction: count = " + openTrasactionCalls + ", isActive = " + result);
+    debugLog("Open transaction: count = " + openTransactionCalls + ", isActive = " + result);
     return result;
   }
 
@@ -576,24 +576,24 @@ public class ObjectStore implements RawStore, Configurable {
       debugLog("Commit transaction: rollback");
       return false;
     }
-    if (openTrasactionCalls <= 0) {
+    if (openTransactionCalls <= 0) {
       RuntimeException e = new RuntimeException("commitTransaction was called but openTransactionCalls = "
-          + openTrasactionCalls + ". This probably indicates that there are unbalanced " +
+          + openTransactionCalls + ". This probably indicates that there are unbalanced " +
           "calls to openTransaction/commitTransaction");
       LOG.error("Unbalanced calls to open/commit Transaction", e);
       throw e;
     }
     if (!currentTransaction.isActive()) {
       RuntimeException e = new RuntimeException("commitTransaction was called but openTransactionCalls = "
-          + openTrasactionCalls + ". This probably indicates that there are unbalanced " +
+          + openTransactionCalls + ". This probably indicates that there are unbalanced " +
           "calls to openTransaction/commitTransaction");
       LOG.error("Unbalanced calls to open/commit Transaction", e);
       throw e;
     }
-    openTrasactionCalls--;
-    debugLog("Commit transaction: count = " + openTrasactionCalls + ", isactive "+ currentTransaction.isActive());
+    openTransactionCalls--;
+    debugLog("Commit transaction: count = " + openTransactionCalls + ", isactive "+ currentTransaction.isActive());
 
-    if ((openTrasactionCalls == 0) && currentTransaction.isActive()) {
+    if ((openTransactionCalls == 0) && currentTransaction.isActive()) {
       transactionStatus = TXN_STATUS.COMMITTED;
       currentTransaction.commit();
     }
@@ -617,8 +617,8 @@ public class ObjectStore implements RawStore, Configurable {
    */
   @Override
   public void rollbackTransaction() {
-    if (openTrasactionCalls < 1) {
-      debugLog("rolling back transaction: no open transactions: " + openTrasactionCalls);
+    if (openTransactionCalls < 1) {
+      debugLog("rolling back transaction: no open transactions: " + openTransactionCalls);
       return;
     }
     debugLog("Rollback transaction, isActive: " + isActiveTransaction());
@@ -627,7 +627,7 @@ public class ObjectStore implements RawStore, Configurable {
         currentTransaction.rollback();
       }
     } finally {
-      openTrasactionCalls = 0;
+      openTransactionCalls = 0;
       transactionStatus = TXN_STATUS.ROLLBACK;
       // remove all detached objects from the cache, since the transaction is
       // being rolled back they are no longer relevant, and this prevents them
