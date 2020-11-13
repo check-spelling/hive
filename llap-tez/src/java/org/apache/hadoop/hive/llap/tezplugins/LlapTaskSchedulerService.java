6592c53092c850bac9edf5d393e5ac29ba3294ee
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 20:15:43 -0500

    spelling: queue
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- llap-tez/src/java/org/apache/hadoop/hive/llap/tezplugins/LlapTaskSchedulerService.java
+++ llap-tez/src/java/org/apache/hadoop/hive/llap/tezplugins/LlapTaskSchedulerService.java
@@ -2574,7 +2574,7 @@ public class LlapTaskSchedulerService extends TaskScheduler {
 
       int oldNumSchedulableTasks = numSchedulableTasks;
       if (numSchedulableTasksConf == 0) {
-        int pendingQueueuCapacity = 0;
+        int pendingQueueCapacity = 0;
         String pendingQueueCapacityString = serviceInstance.getProperties()
                 .get(LlapRegistryService.LLAP_DAEMON_TASK_SCHEDULER_ENABLED_WAIT_QUEUE_SIZE);
         if (pendingQueueCapacityString == null) {
@@ -2585,9 +2585,9 @@ public class LlapTaskSchedulerService extends TaskScheduler {
                 serviceInstance, serviceInstance.getResource().getVirtualCores(),
                 pendingQueueCapacityString, serviceInstance.getResource().getMemory());
         if (pendingQueueCapacityString != null) {
-          pendingQueueuCapacity = Integer.parseInt(pendingQueueCapacityString);
+          pendingQueueCapacity = Integer.parseInt(pendingQueueCapacityString);
         }
-        this.numSchedulableTasks = numVcores + pendingQueueuCapacity;
+        this.numSchedulableTasks = numVcores + pendingQueueCapacity;
       } else {
         this.numSchedulableTasks = numSchedulableTasksConf;
         LOG.info("Setting up node: " + serviceInstance + " with schedulableCapacity=" + this.numSchedulableTasks);
