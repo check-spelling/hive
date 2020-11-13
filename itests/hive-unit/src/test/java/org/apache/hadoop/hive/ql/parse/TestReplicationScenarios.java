e1296ad42bb4761bfb1b4d0c1b8687b191a2f817
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 21:11:12 -0500

    spelling: unsuccessful
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenarios.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenarios.java
@@ -2280,7 +2280,7 @@ public class TestReplicationScenarios {
     try {
       Thread.sleep(cleanerIntervalSeconds * 1000 * 10);
     } catch (InterruptedException e) {
-      LOG.warn("Sleep unsuccesful", e);
+      LOG.warn("Sleep unsuccessful", e);
     }
 
     //verify events get replicated
@@ -2306,7 +2306,7 @@ public class TestReplicationScenarios {
     try {
       Thread.sleep(cleanerIntervalSeconds * 1000 * 10);
     } catch (InterruptedException e) {
-      LOG.warn("Sleep unsuccesful", e);
+      LOG.warn("Sleep unsuccessful", e);
     }
 
     incrDump = replDumpDb(dbName);
@@ -2334,7 +2334,7 @@ public class TestReplicationScenarios {
     try {
       Thread.sleep(cleanerIntervalSeconds * 1000 * 10);
     } catch (InterruptedException e) {
-      LOG.warn("Sleep unsuccesful", e);
+      LOG.warn("Sleep unsuccessful", e);
     }
 
     //check replication success
@@ -2357,7 +2357,7 @@ public class TestReplicationScenarios {
     try {
       Thread.sleep(cleanerIntervalSeconds * 1000 * 10);
     } catch (InterruptedException e) {
-      LOG.warn("Sleep unsuccesful", e);
+      LOG.warn("Sleep unsuccessful", e);
     }
 
     //events should be deleted before dump
