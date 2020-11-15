a4f363872274c11e5c07f6f5f3d26d13fac5d873
Josh Soref <jsoref@users.noreply.github.com>
2020-11-14 23:32:40 -0500

    spelling: partition
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/stats/annotation/StatsRulesProcFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/stats/annotation/StatsRulesProcFactory.java
@@ -1242,7 +1242,7 @@ public class StatsRulesProcFactory {
 
               // if column name is not contained in needed column list then it
               // is a partition column. We do not need to evaluate partition columns
-              // in filter expression since it will be taken care by partitio pruner
+              // in filter expression since it will be taken care by partition pruner
               if (neededCols != null && !neededCols.contains(colName)) {
                 return numRows;
               }
@@ -1263,7 +1263,7 @@ public class StatsRulesProcFactory {
 
                 // if column name is not contained in needed column list then it
                 // is a partition column. We do not need to evaluate partition columns
-                // in filter expression since it will be taken care by partitio pruner
+                // in filter expression since it will be taken care by partition pruner
                 if (neededCols != null && neededCols.indexOf(colName) == -1) {
                   return numRows;
                 }
